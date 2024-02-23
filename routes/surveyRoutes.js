f473c8b33ade34ec0dca2fbcce22a89ed8018e4e
alex <alex87gonz11@gmail.com>
2024-02-23 09:01:55 +0100

    feat:recover all surveys. Route: /api/surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireCredits = require('../middlewares/requireCredits');
 const requireLogin = require('../middlewares/requireLogin');
@@ -13,10 +16,6 @@ module.exports = (app) => {
     requireCredits,
     async (req, res) => {
       const { title, subject, body, recipients } = req.body;
-      console.log(req.body);
-      console.log(title);
-      console.log(subject);
-      console.log(recipients);
       const survey = new Survey({
         title,
         subject,
@@ -43,7 +42,63 @@ module.exports = (app) => {
     }
   );
 
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.post('/api/surveys/webhooks', async (req, res) => {
+    const p = new Path('/api/surveys/:surveyid/:choice');
+    const updates = [];
+    const event = _.chain(req.body)
+      .map(({ email, url }) => {
+        const path = new URL(url).pathname;
+        const pathInfo = p.test(path);
+        if (pathInfo)
+          return {
+            email,
+            surveyId: pathInfo.surveyid,
+            choice: pathInfo.choice,
+          };
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        // Construir las actualizaciones pero no ejecutarlas todavía
+        updates.push({
+          updateOne: {
+            filter: {
+              _id: surveyId,
+              recipients: { $elemMatch: { email: email, voted: false } },
+            },
+            update: {
+              $inc: { [choice]: 1 },
+              $set: { 'recipients.$.voted': true },
+              lastResponded: new Date(),
+            },
+          },
+        });
+      })
+      .value();
+
+    try {
+      // Ejecutar todas las actualizaciones en una sola operación
+      const respond = await Survey.bulkWrite(updates);
+      console.log(respond);
+      res.send({});
+    } catch (error) {
+      res
+        .status(500)
+        .send('Error al procesar las respuestas de las encuestas.');
+    }
+  });
+
+  app.get('/api/surveys', async (req, res) => {
+    try {
+      res.send(
+        await Survey.find({_user : req.user.id}).select({ recipients: false })
+      );
+    } catch (error) {
+      res.status(500).send(`Error: ${error}`);
+    }
+  });
+
+  app.get('/api/surveys/:surveiId/:choice', (req, res) => {
     res.send('Thanks for voting!!!');
   });
 };
