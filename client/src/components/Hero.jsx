0dd5d3c95e113838de77566f8fa88ba2279d94d3
Luis Alejandro <alex87gonz11@gmail.com>
2024-04-27 11:14:44 +0000

    fix: landing styling

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,6 @@
- 
-
-import { Box, Typography } from '@mui/material';
-import heroImage from '../assets/hero.png';
-import Styles from '../assets/Styles';
+import { Box, Typography } from "@mui/material";
+import heroImage from "../assets/hero.png";
+import Styles from "../assets/Styles";
 
 export const Hero = () => {
   return (
@@ -10,42 +8,58 @@ export const Hero = () => {
       sx={{
         ...Styles.heroBox,
         backgroundImage: `url(${heroImage})`,
-        position: 'relative',
-        textAlign: 'center',
-        height: '100vh', // Establecemos la altura para ocupar toda la altura de la ventana
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center', // Centramos verticalmente el contenido
+        // backgroundPosition: "center", 
+        // backgroundRepeat: "no-repeat",
+         backgroundSize: "cover",
+        position: "relative",
+        textAlign: "center",
+        height: "100vh", 
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center", 
+        textAlign: "center",
       }}
     >
-      <Typography
-        variant="h2"
+      <Box
         sx={{
-          ...Styles.asideTitleTypography,
-          color: 'white',
-          position: 'absolute',
-          top: '5%',
+          position: "absolute",
+          top: "5%",
           left: 0,
           right: 0,
-          
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center", 
 
         }}
       >
-        Simply Online Survey Software
-      </Typography>
-      <Typography
-        variant="h4"
-        sx={{
-          ...Styles.asideTitleTypography,
-          position: 'absolute',
-          top: '20%',
-          left: 0,
-          right: 0,
-          marginTop: '5%',
-        }}
-      >
-        From Feedback to Strategy: Your Survey Solution
-      </Typography>
+        <Typography
+          variant="h2"
+          gutterBottom
+          sx={{
+            ...Styles.asideTitleTypography,
+            color: "white",
+            fontSize: { xs: "2rem", md: "3rem" },
+            
+          }}
+        >
+          Simply Online Survey Software
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{
+            ...Styles.asideTitleTypography,
+            fontSize: { xs: "2rem", md: "3rem" }
+            // position: "absolute",
+            // top: "20%",
+            // left: 0,
+            // right: 0,
+            // marginTop: "5%",
+          }}
+        >
+          From Feedback to Strategy: Your Survey Solution
+        </Typography>
+      </Box>
     </Box>
   );
 };
--- client/src/pages/SurveyForm.jsx
+++ client/src/pages/SurveyForm.jsx
@@ -1,29 +1,37 @@
-import React, { useState } from 'react';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { Box, Container, Typography } from '@mui/material';
-import { useAddSurveyMutation } from '../store';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import {
+  Box,
+  Container,
+  Typography,
+  ListItem,
+  ListItemText,
+  Divider,
+  List,
+} from "@mui/material";
+import { useAddSurveyMutation } from "../store";
+import { useNavigate } from "react-router-dom";
 
 const validationSchema = yup.object({
   body: yup
-    .string('Enter email`s body')
-    .min(8, 'Body should be of minimum 8 characters length')
-    .required('Body is required'),
+    .string("Enter email`s body")
+    .min(8, "Body should be of minimum 8 characters length")
+    .required("Body is required"),
   recipients: yup
     .array()
-    .transform((value) => value.trim().split(',')) // Handle spaces around commas
-    .of(yup.string().email('Enter a valid email').required())
-    .required('Email is required'),
+    .transform((value) => value.trim().split(",")) // Handle spaces around commas
+    .of(yup.string().email("Enter a valid email").required())
+    .required("Email is required"),
   campain: yup
-    .string('Enter the campain`s title')
-    .min(8, 'Title should be of minimum 8 characters length')
-    .required('Campain must have a title'),
+    .string("Enter the campain`s title")
+    .min(8, "Title should be of minimum 8 characters length")
+    .required("Campain must have a title"),
   subject: yup
-    .string('Enter the email`s subject')
-    .required('Campain must have a subject'),
+    .string("Enter the email`s subject")
+    .required("Campain must have a subject"),
 });
 
 export const SurveyForm = ({ refetchUsers }) => {
@@ -33,10 +41,10 @@ export const SurveyForm = ({ refetchUsers }) => {
 
   const formik = useFormik({
     initialValues: {
-      body: 'My email`s body',
-      recipients: 'foo@foo.com,other@foo.com,another@other.com,...',
-      campain: 'My campain',
-      subject: 'Email`s subject',
+      body: "My email`s body",
+      recipients: "foo@foo.com,other@foo.com,another@other.com,...",
+      campain: "My campain",
+      subject: "Email`s subject",
     },
     validationSchema: validationSchema,
     onSubmit: async (values) => {
@@ -53,15 +61,15 @@ export const SurveyForm = ({ refetchUsers }) => {
       try {
         await addSurvey(survey);
         refetchUsers();
-        navigate('/surveys');
+        navigate("/surveys");
       } catch (error) {
-        navigate('/servererror', { state: error });
+        navigate("/servererror", { state: error });
       }
     },
   });
 
   return (
-    <Container maxWidth="md" sx={{ mt: '15px' }}>
+    <Container maxWidth="md" sx={{ mt: "15px" }}>
       <form onSubmit={formik.handleSubmit}>
         {step === 0 && (
           <Box>
@@ -74,12 +82,12 @@ export const SurveyForm = ({ refetchUsers }) => {
               value={formik.values.campain}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              onFocus={() => formik.setFieldValue('campain', '')}
+              onFocus={() => formik.setFieldValue("campain", "")}
               error={formik.touched.campain && Boolean(formik.errors.campain)}
               helperText={formik.touched.campain && formik.errors.campain}
             />
             <TextField
-              sx={{ mt: '8px' }}
+              sx={{ mt: "8px" }}
               fullWidth
               id="subject"
               name="subject"
@@ -88,12 +96,12 @@ export const SurveyForm = ({ refetchUsers }) => {
               value={formik.values.subject}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              onFocus={() => formik.setFieldValue('subject', '')}
+              onFocus={() => formik.setFieldValue("subject", "")}
               error={formik.touched.subject && Boolean(formik.errors.subject)}
               helperText={formik.touched.subject && formik.errors.subject}
             />
             <TextField
-              sx={{ mt: '8px' }}
+              sx={{ mt: "8px" }}
               fullWidth
               id="body"
               name="body"
@@ -102,12 +110,12 @@ export const SurveyForm = ({ refetchUsers }) => {
               value={formik.values.body}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              onFocus={() => formik.setFieldValue('body', '')}
+              onFocus={() => formik.setFieldValue("body", "")}
               error={formik.touched.body && Boolean(formik.errors.body)}
               helperText={formik.touched.body && formik.errors.body}
             />
             <TextField
-              sx={{ mt: '8px' }}
+              sx={{ mt: "8px" }}
               fullWidth
               id="recipients"
               name="recipients"
@@ -116,7 +124,7 @@ export const SurveyForm = ({ refetchUsers }) => {
               value={formik.values.recipients}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              onFocus={() => formik.setFieldValue('recipients', '')}
+              onFocus={() => formik.setFieldValue("recipients", "")}
               error={
                 formik.touched.recipients && Boolean(formik.errors.recipients)
               }
@@ -125,39 +133,73 @@ export const SurveyForm = ({ refetchUsers }) => {
           </Box>
         )}
         {step === 1 && (
+          // <Box>
+          //   <Typography variant="h4" gutterBottom>
+          //     Please confirm your entries
+          //   </Typography>
+          //   <Box sx={{ mb: '8px' }}>
+          //     <Typography variant="h6" gutterBottom>
+          //       Campain Title
+          //     </Typography>
+          //     <Typography variant="body1">{formik.values.campain}</Typography>
+          //   </Box>
+          //   <Box sx={{ mb: '8px' }}>
+          //     <Typography variant="h6" gutterBottom>
+          //       Subject
+          //     </Typography>
+          //     <Typography variant="body1">{formik.values.subject}</Typography>
+          //   </Box>
+          //   <Box sx={{ mb: '8px' }}>
+          //     <Typography variant="h6" gutterBottom>
+          //       Email body
+          //     </Typography>
+          //     <Typography variant="body1">{formik.values.body}</Typography>
+          //   </Box>
+          //   <Box sx={{ mb: '8px' }}>
+          //     <Typography variant="h6" gutterBottom>
+          //       Recipients list
+          //     </Typography>
+          //     <Typography variant="body1">
+          //       {formik.values.recipients}
+          //     </Typography>
+          //   </Box>
+          // </Box>
           <Box>
             <Typography variant="h4" gutterBottom>
               Please confirm your entries
             </Typography>
-            <Box sx={{ mb: '8px' }}>
-              <Typography variant="h6" gutterBottom>
-                Campain Title
-              </Typography>
-              <Typography variant="body1">{formik.values.campain}</Typography>
-            </Box>
-            <Box sx={{ mb: '8px' }}>
-              <Typography variant="h6" gutterBottom>
-                Subject
-              </Typography>
-              <Typography variant="body1">{formik.values.subject}</Typography>
-            </Box>
-            <Box sx={{ mb: '8px' }}>
-              <Typography variant="h6" gutterBottom>
-                Email body
-              </Typography>
-              <Typography variant="body1">{formik.values.body}</Typography>
-            </Box>
-            <Box sx={{ mb: '8px' }}>
-              <Typography variant="h6" gutterBottom>
-                Recipients list
-              </Typography>
-              <Typography variant="body1">
-                {formik.values.recipients}
-              </Typography>
-            </Box>
+            <List>
+              <ListItem>
+                <ListItemText
+                  primary="Campain Title"
+                  secondary={formik.values.campain}
+                />
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <ListItemText
+                  primary="Subject"
+                  secondary={formik.values.subject}
+                />
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <ListItemText
+                  primary="Email body"
+                  secondary={formik.values.body}
+                />
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <ListItemText
+                  primary="Recipients list"
+                  secondary={formik.values.recipients}
+                />
+              </ListItem>
+            </List>
           </Box>
         )}
-        <Box display="flex" justifyContent="space-between" sx={{ mt: '8px' }}>
+        <Box display="flex" justifyContent="space-between" sx={{ mt: "8px" }}>
           {step === 1 && (
             <Button
               variant="contained"
@@ -168,7 +210,7 @@ export const SurveyForm = ({ refetchUsers }) => {
             </Button>
           )}
           <Button color="primary" variant="contained" type="submit">
-            {step === 0 ? 'Next' : 'Submit'}
+            {step === 0 ? "Next" : "Submit"}
           </Button>
         </Box>
       </form>
